ae343777b44ff55a1b8d68327668be4fa3e7a930
Thunchanok Kakaew <thunchanokkakaew@MacBook-Air-khxng-Thunchanok.local>
2026-04-28 15:23:09 +0700

    ใส่ข้อความ

--- firebase/config.ts
+++ firebase/config.ts
@@ -8,7 +8,6 @@ const firebaseConfig = {
   apiKey: 'AIzaSyAxHCW0stUNJkb-H7aNCDoYTS11QDybVyI',
   authDomain: 'nomnomuni-97584.firebaseapp.com',
   projectId: 'nomnomuni-97584',
-  storageBucket: 'nomnomuni-97584.firebasestorage.app',   // ← แก้ให้ถูก
   messagingSenderId: '133034446233',
   appId: '1:133034446223:web:297b1badca58d42e168ffa',
   measurementId: 'G-J6VPM4EQ6X',
