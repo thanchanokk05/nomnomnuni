63c57050663ccf5c2cb679503fbb4bf5e2748196
Thunchanok Kakaew <thunchanokkakaew@MacBook-Air-khxng-Thunchanok.local>
2026-04-28 15:36:52 +0700

    ใส่ข้อความ

--- src/firebase.js
+++ src/firebase.js
@@ -12,8 +12,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAxHCW0stUNJkb-H7aNCDoYTS11QDybVyI",
   authDomain: "nomnomuni-97584.firebaseapp.com",
   projectId: "nomnomuni-97584",
-  storageBucket: "nomnomuni-97584.firebasestorage.app",
-  messagingSenderId: "133034446223",
+messagingSenderId: "133034446223",
   appId: "1:133034446223:web:297b1badca58d42e168ffa",
   measurementId: "G-J6VPM4EQ6X",
 };
